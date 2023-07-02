42e96f58090316d0cc87d0ea82c329f0df384544
AllanSechrist <sechristallan@gmail.com>
2023-07-02 09:48:31 +0900

    add books app

---
+++ books/models.py
@@ -0,0 +1,22 @@
+import uuid
+from django.db import models
+from django.urls import reverse
+
+
+
+class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    title = models.CharField(max_length=500)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    level = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[str(self.id)])
