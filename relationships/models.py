96f9de78dac439fb54a0840271ef9a4d05f7c170
AllanSechrist <sechristallan@gmail.com>
2023-07-22 17:42:17 +0900

    refactor relationships models

--- relationships/models.py
+++ relationships/models.py
@@ -1,47 +1,63 @@
 from django.db import models
 
 
-class StudentBookRelation(models.Model):
+class ClassEnrollment(models.Model):
     """
-    A model that connects students, books, and courses.
-    Courses will have specific books that are connected
-    to the students of that course.
+    Used to create a relationship between students
+    and classes
     """
     student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
     book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
-    textbook = models.ForeignKey("books.Textbook", on_delete=models.CASCADE)
-    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
+
+
+class BookAssignments(models.Model):
+    """
+    Used to create a relationship between books and classes
+    """
+    related_class = models.ForeignKey("classes.Class", on_delete=models.CASCADE)
+    book = models.ForeignKey("book.Books", on_delete=models.CASCADE)
+
+# class StudentBookRelation(models.Model):
+#     """
+#     A model that connects students, books, and courses.
+#     Courses will have specific books that are connected
+#     to the students of that course.
+#     """
+#     student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
+#     book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+#     textbook = models.ForeignKey("books.Textbook", on_delete=models.CASCADE)
+#     course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
     
-    def __str__(self):
-        return f"{self.student} - {self.book} - {self.textbook} - {self.course}"
+#     def __str__(self):
+#         return f"{self.student} - {self.book} - {self.textbook} - {self.course}"
     
 
-class ReaderBookHistory(models.Model):
-    """
-    A model that keeps track of the readers a student
-    has been assigned. 
-    """
-    student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
-    reader = models.ForeignKey("books.Book", on_delete=models.CASCADE)
-    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
-    start_date = models.DateField()
-    finish_date = models.DateField(null=True, blank=True)
+# class ReaderBookHistory(models.Model):
+#     """
+#     A model that keeps track of the readers a student
+#     has been assigned. 
+#     """
+#     student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
+#     reader = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+#     course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
+#     start_date = models.DateField()
+#     finish_date = models.DateField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.student} - {self.reader} - {self.start_date} - {self.finish_date}"
+#     def __str__(self):
+#         return f"{self.student} - {self.reader} - {self.start_date} - {self.finish_date}"
     
 
-class TextbookHistory(models.Model):
-    """
-    A model that keeps track of the textbooks a student
-    has been assigned. 
-    """
-    student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
-    textbook = models.ForeignKey("books.TextBook", on_delete=models.CASCADE)
-    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
-    start_date = models.DateField()
-    finish_date = models.DateField(null=True, blank=True)
+# class TextbookHistory(models.Model):
+#     """
+#     A model that keeps track of the textbooks a student
+#     has been assigned. 
+#     """
+#     student = models.ForeignKey("students.Student", on_delete=models.CASCADE)
+#     textbook = models.ForeignKey("books.TextBook", on_delete=models.CASCADE)
+#     course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
+#     start_date = models.DateField()
+#     finish_date = models.DateField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.student} - {self.textbook} - {self.start_date} - {self.finish_date}"
+#     def __str__(self):
+#         return f"{self.student} - {self.textbook} - {self.start_date} - {self.finish_date}"
     
